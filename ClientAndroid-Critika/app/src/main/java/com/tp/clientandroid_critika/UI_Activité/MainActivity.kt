cfb464e380e2910afa72c8b2d2b7ffe213321423
William Alexand Hernandez <whernandez@dept-info.crosemont.quebec>
2021-11-08 11:52:52 -0500

    Suppression de l'attribut droit dans la classe Utilisateur et dans les UtilisateurTest.kt

---
+++ ClientAndroid-Critika/app/src/main/java/com/tp/clientandroid_critika/UI_Activité/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tp.clientandroid_critika.UI_Activité
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tp.clientandroid_critika.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
