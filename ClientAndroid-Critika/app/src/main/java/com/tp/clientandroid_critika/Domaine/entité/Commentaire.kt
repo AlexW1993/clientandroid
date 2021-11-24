1936b13f83a6e63c81bfe4ed712fd04c7853de4a
yevgeniy shmygun <yshmygun@dept-info.crosemont.quebec>
2021-11-24 01:21:13 -0500

    javadoc et constructeur secondaire pour la classe Commentaire.kt

--- ClientAndroid-Critika/app/src/main/java/com/tp/clientandroid_critika/Domaine/entité/Commentaire.kt
+++ ClientAndroid-Critika/app/src/main/java/com/tp/clientandroid_critika/Domaine/entité/Commentaire.kt
@@ -2,10 +2,25 @@ package com.tp.clientandroid_critika.Domaine.entité
 
 import java.time.LocalDateTime
 
+/**
+ * Classe Commentaire
+ *
+ * Représente un commentaire pour un jeu vidéo laissé par un utilisateur
+ */
+class Commentaire(
+    var id : String,
+    var jeuVideoId : String,
+    var utilisateurId : String,
+    var dateHeure : LocalDateTime?,
+    var contenue : String,
+    var utilisateur : Utilisateur?
+    ){
 
-class Commentaire(var id : String, var jeuVideoId : String, var utilisateurId : String, var dateHeure : LocalDateTime,
-                  var contenue : String, var utilisateur : Utilisateur?){
-
+    /**
+     * Constructeur secondaire pour la classe commentaire avec les valeurs par defaut
+     */
+    constructor() : this("","","",null,"",null)
+    
     override fun toString(): String {
         return "Commentaire(id='$id', jeuVideoId='$jeuVideoId', utilisateurId='$utilisateurId', dateHeure=$dateHeure, contenue='$contenue', utilisateur=$utilisateur)"
     }
