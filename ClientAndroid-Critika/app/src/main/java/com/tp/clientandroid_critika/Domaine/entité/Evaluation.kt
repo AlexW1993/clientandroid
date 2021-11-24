c1c7f49c610f99918b96afa3baa9418e8e8bec3e
yevgeniy shmygun <yshmygun@dept-info.crosemont.quebec>
2021-11-24 01:18:37 -0500

    javadoc et constructeur secondaire pour la classe Evaluation.kt

--- ClientAndroid-Critika/app/src/main/java/com/tp/clientandroid_critika/Domaine/entité/Evaluation.kt
+++ ClientAndroid-Critika/app/src/main/java/com/tp/clientandroid_critika/Domaine/entité/Evaluation.kt
@@ -1,6 +1,21 @@
 package com.tp.clientandroid_critika.Domaine.entité
 
-class Evaluation (var id : String, var jeuVideoId : String, var idUtilisateur : String, var note : Int) {
+/**
+ * Classe Évaluation
+ *
+ * Représente une évaluation pour un jeu vidéo
+ */
+class Evaluation (
+    var id : String,
+    var jeuVideoId : String,
+    var idUtilisateur : String,
+    var note : Int
+    ) {
+
+    /**
+     * Constructeur secondaire avec les valeurs par defaut
+     */
+    constructor() : this("","","",0)
 
     override fun toString(): String {
         return "Evaluation(id='$id', idJeu='$jeuVideoId', idUtilisateur='$idUtilisateur', note=$note)"
