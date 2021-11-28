3e959e39c5d3bdda571a55bef4b01b315d3319ea
yevgeniy shmygun <yshmygun@dept-info.crosemont.quebec>
2021-11-28 10:46:29 -0500

    vue vue/VuePageJeu.kt est fait

--- ClientAndroid-Critika/app/src/main/java/com/tp/clientandroid_critika/Présentation/vue/VuePageJeu.kt
+++ ClientAndroid-Critika/app/src/main/java/com/tp/clientandroid_critika/Présentation/vue/VuePageJeu.kt
@@ -5,9 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
+import com.tp.clientandroid_critika.Domaine.entité.JeuVideo
+import com.tp.clientandroid_critika.Présentation.contrat.IContratPrésentateurVuePageJeu
+import com.tp.clientandroid_critika.Présentation.présenteur.PrésentateurPageJeu
+
 import com.tp.clientandroid_critika.R
 
-class VuePageJeu : Fragment() {
+class VuePageJeu : Fragment(), IContratPrésentateurVuePageJeu.IVuePageJeu {
+
+    private var _présentateur : PrésentateurPageJeu? = null
+    private var _nav : NavController? = null
+    private var _btnDéconnection : ImageButton? = null
+    private var _btnMenuPrincipale : ImageButton? = null
+    private var _btnRecherche : ImageButton? = null
+    private var _btnCompte : ImageButton? = null
+    private var _titreJeu : TextView? = null
+    private var _anneeJeu : TextView? = null
+    private var _moyenneJeu : TextView? = null
+    private var _imageJeu : ImageView? = null
+    private var _descriptionJeu : TextView? = null
+    private var _listeCommentaires : RecyclerView? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +45,35 @@ class VuePageJeu : Fragment() {
         return inflater.inflate(R.layout.fragment_page_jeu, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _présentateur = PrésentateurPageJeu(this)
+        _nav = Navigation.findNavController(view)
+        _btnDéconnection = view.findViewById(R.id.bouton_deconnection)
+        _btnMenuPrincipale = view.findViewById(R.id.bouton_menu)
+        _btnRecherche = view.findViewById(R.id.bouton_recherche)
+        _btnCompte = view.findViewById(R.id.bouton_compte)
+        _titreJeu = view.findViewById(R.id.titre_jeu)
+        _anneeJeu = view.findViewById(R.id.annee_jeu)
+        _moyenneJeu = view.findViewById(R.id.moyenne_jeu)
+        _imageJeu = view.findViewById(R.id.image_jeu)
+        _descriptionJeu = view.findViewById(R.id.description_jeu)
+        _listeCommentaires = view.findViewById(R.id.commentaires_jeu)
+        _btnDéconnection?.setOnClickListener {
+                view -> _nav!!.navigate(R.id.vuePageInitiale)
+        }
+        _btnMenuPrincipale?.setOnClickListener {
+                view -> _nav!!.navigate(R.id.vueMenuPrincipale)
+        }
+        _btnRecherche?.setOnClickListener {
+                view -> _nav!!.navigate(R.id.vuePageRecherche)
+        }
+        _btnCompte?.setOnClickListener {
+                view -> _nav!!.navigate(R.id.vueMenuCompte)
+        }
+        _présentateur?.chercherInformationJeuSelectionné()
+    }
+
     companion object {
 
         @JvmStatic
@@ -28,4 +81,13 @@ class VuePageJeu : Fragment() {
             VuePageJeu().apply {
             }
     }
+
+    override fun affichageInformationJeuSelecionné(jeu: JeuVideo?) {
+        _titreJeu?.text = jeu?.nom
+        _anneeJeu?.text = jeu?.anneSortie.toString()
+        _moyenneJeu?.text = jeu?.calculerMoyenneEvaluation().toString()
+        _imageJeu = null
+        _descriptionJeu?.text = jeu?.description
+    }
+
 }
