55296fa2b8b34093fd6147fda2293374bb2cb105
yevgeniy shmygun <yshmygun@dept-info.crosemont.quebec>
2021-11-30 22:55:32 -0500

    ajout evaluation par interacteur/GestionEvaluation.kt ajout evaluation dans la Interface SourceDeDonnées.kt

--- ClientAndroid-Critika/app/src/main/java/com/tp/clientandroid_critika/Domaine/interacteur/GestionEvaluation.kt
+++ ClientAndroid-Critika/app/src/main/java/com/tp/clientandroid_critika/Domaine/interacteur/GestionEvaluation.kt
@@ -4,7 +4,7 @@ import com.tp.clientandroid_critika.Domaine.entité.Evaluation
 
 class GestionEvaluation (var _source: SourceDeDonnées?){
 
-    fun ajouterEvaluation(evaluation: Evaluation){
-        return
+    fun ajouterEvaluation(evaluation: Evaluation) : Boolean?{
+        return _source?.ajouterEvaluation(evaluation)
     }
 }
