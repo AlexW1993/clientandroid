292b2363c9d1d1bac4ecb6ad12da227ac0b3fbbf
whernandez <whernandez@dept-info.crosemont.quebec>
2021-12-02 17:12:53 -0500

    Javadoc dans GestionEvaluation.kt

--- ClientAndroid-Critika/app/src/main/java/com/tp/clientandroid_critika/Domaine/interacteur/GestionEvaluation.kt
+++ ClientAndroid-Critika/app/src/main/java/com/tp/clientandroid_critika/Domaine/interacteur/GestionEvaluation.kt
@@ -4,10 +4,24 @@ import com.tp.clientandroid_critika.Domaine.entité.Evaluation
 
 class GestionEvaluation(var _source: SourceDeDonnées?) {
 
+    /**
+     * La méthode permet de ajouter un note d'un utilisateur à un jeu
+     *
+     * @param (evaluation: Evaluation), L'évalaution a ajouté
+     *
+     * @return (Boolean?) true por confirmer que l'évalaution a été ajouté correctement, false si il y a eu un erreur
+     */
     fun ajouterEvaluation(evaluation: Evaluation): Boolean? {
         return _source?.ajouterEvaluation(evaluation)
     }
 
+    /**
+     * La méthode permet de modifier un note que l'utilisateur a fait à un jeu
+     *
+     * @param (evaluation: Evaluation), L'évalaution a modifié
+     *
+     * @return (Boolean?) true por confirmer que l'évalaution a été modifié correctement, false si il y a eu un erreur
+     */
     fun modifierEvaluation(evaluation: Evaluation): Boolean? {
         return _source?.modifierEvaluation(evaluation)
     }
