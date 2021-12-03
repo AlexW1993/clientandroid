bb8069d73db7a1e7edab54bd23529021251a44dc
whernandez <whernandez@dept-info.crosemont.quebec>
2021-12-03 10:45:29 -0500

    Les test pour les functions GestionJeuVideo passent correctement

--- ClientAndroid-Critika/app/src/main/java/com/tp/clientandroid_critika/SourceDeDonnées/SourceDeDonnéesBidon.kt
+++ ClientAndroid-Critika/app/src/main/java/com/tp/clientandroid_critika/SourceDeDonnées/SourceDeDonnéesBidon.kt
@@ -6,22 +6,78 @@ import com.tp.clientandroid_critika.Domaine.entité.JeuVideo
 import com.tp.clientandroid_critika.Domaine.entité.Utilisateur
 import com.tp.clientandroid_critika.Domaine.interacteur.SourceDeDonnées
 
-class SourceDeDonnéesBidon : SourceDeDonnées{
+class SourceDeDonnéesBidon : SourceDeDonnées {
 
     override fun chercherUtilisateur(u: Utilisateur): Utilisateur? {
         return u
     }
 
     override fun ajouterUtilisateur(utilisateur: Utilisateur): Boolean {
-       return true
+        return true
     }
 
     override fun chercherTousJeux(): List<JeuVideo>? {
-        TODO("Not yet implemented")
+        var listeJeux: MutableList<JeuVideo> = ArrayList()
+        var listeEvaluation1: MutableList<Evaluation> = ArrayList()
+        var listeEvaluation2: MutableList<Evaluation> = ArrayList()
+        listeEvaluation1?.add(Evaluation("aaa", "aaa", "aaa", 5))
+        listeEvaluation2?.add(Evaluation("bbb", "bbb", "bbb", 3))
+        var jeuVideo = JeuVideo(
+            "aaaa", "test", "un test", "Test1", "Test", "Solo",
+            2021, null, listeEvaluation1
+        )
+        listeJeux?.add(jeuVideo)
+        jeuVideo = JeuVideo(
+            "bbbb", "test", "un test", "Test2", "Test", "Solo",
+            2021, null, null
+        )
+        listeJeux?.add(jeuVideo)
+        jeuVideo = JeuVideo(
+            "cccc", "test", "un test", "Test1", "Test", "Solo",
+            2021, null, listeEvaluation2
+        )
+        listeJeux?.add(jeuVideo)
+        jeuVideo = JeuVideo(
+            "dddd", "test", "un test", "Test2", "Test", "Solo",
+            2021, null, null
+        )
+        listeJeux?.add(jeuVideo)
+        return listeJeux
     }
 
     override fun chercherTousJeuxParPlateforme(plateforme: String): List<JeuVideo>? {
-        TODO("Not yet implemented")
+        var listeJeux: MutableList<JeuVideo> = ArrayList()
+        var listeEvaluation1: MutableList<Evaluation> = ArrayList()
+        var listeEvaluation2: MutableList<Evaluation> = ArrayList()
+        listeEvaluation1?.add(Evaluation("aaa", "aaa", "aaa", 5))
+        listeEvaluation2?.add(Evaluation("bbb", "bbb", "bbb", 3))
+        var jeuVideo = JeuVideo(
+            "aaaa", "test", "un test", "Test1", "Test", "Solo",
+            2021, null, listeEvaluation1
+        )
+        listeJeux?.add(jeuVideo)
+        jeuVideo = JeuVideo(
+            "bbbb", "test", "un test", "Test2", "Test", "Solo",
+            2021, null, null
+        )
+        listeJeux?.add(jeuVideo)
+        jeuVideo = JeuVideo(
+            "cccc", "test", "un test", "Test1", "Test", "Solo",
+            2021, null, listeEvaluation2
+        )
+        listeJeux?.add(jeuVideo)
+        jeuVideo = JeuVideo(
+            "dddd", "test", "un test", "Test2", "Test", "Solo",
+            2021, null, null
+        )
+        var listePlateforme: MutableList<JeuVideo> = ArrayList()
+        listeJeux?.add(jeuVideo)
+        for (l in listeJeux) {
+            if (plateforme == l.plateforme) {
+                listePlateforme.add(l)
+            }
+        }
+        return listePlateforme
     }
 
     override fun ajouterCommentaire(commentaire: Commentaire): Boolean {
---
+++ ClientAndroid-Critika/app/src/test/java/com/tp/clientandroid_critika/Domaine/interacteur/GestionJeuVideoTest.kt
@@ -0,0 +1,40 @@
+package com.tp.clientandroid_critika.Domaine.interacteur
+
+import com.tp.clientandroid_critika.Domaine.entité.Commentaire
+import com.tp.clientandroid_critika.Domaine.entité.JeuVideo
+import com.tp.clientandroid_critika.Domaine.entité.Utilisateur
+import com.tp.clientandroid_critika.SourceDeDonnées.SourceDeDonnéesBidon
+import org.junit.Assert
+import org.junit.Test
+import java.time.LocalDateTime
+
+class GestionJeuVideoTest {
+
+    @Test
+    fun `étant donné un utilisateur, lorsque j'ai veux avoir les jeux videos de la appliacation', j'obtien tout las liste des jeux videos`() {
+
+        //Mise en place
+        var cobaye: List<JeuVideo>
+        var sourceDeDonnées = SourceDeDonnéesBidon()
+
+        //Exécution
+        cobaye = GestionJeuxVideo(sourceDeDonnées).chercherTousJeuxVideo()!!
+
+        //Vérification
+        Assert.assertEquals(4, cobaye.size)
+    }
+
+    @Test
+    fun `étant donné un utilisateur, lorsque j'ai veux avoir les jeux videos d'une plateforme', j'obtien les jeux videos qui sont relié à la plateforme`() {
+
+        //Mise en place
+        var cobaye: List<JeuVideo>
+        var sourceDeDonnées = SourceDeDonnéesBidon()
+
+        //Exécution
+        cobaye = GestionJeuxVideo(sourceDeDonnées).chercherJuexVideoParConsole("Test1")!!
+
+        //Vérification
+        Assert.assertEquals(2, cobaye.size)
+    }
+}
