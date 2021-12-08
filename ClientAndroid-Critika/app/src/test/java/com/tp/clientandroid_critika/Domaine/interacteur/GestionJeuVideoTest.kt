5fc3bea366a57ed3cd8c0a254f4e2783fe9a979a
yevgeniy shmygun <yshmygun@dept-info.crosemont.quebec>
2021-12-07 19:25:29 -0500

    Test de funtion chercherJeuxParMotCle du GestionJeuVideoTest.kt passe correctement

--- ClientAndroid-Critika/app/src/test/java/com/tp/clientandroid_critika/Domaine/interacteur/GestionJeuVideoTest.kt
+++ ClientAndroid-Critika/app/src/test/java/com/tp/clientandroid_critika/Domaine/interacteur/GestionJeuVideoTest.kt
@@ -34,4 +34,19 @@ class GestionJeuVideoTest {
         //Vérification
         Assert.assertEquals(2, cobaye.size)
     }
+
+    @Test
+    fun `étant donné un utilisateur, lorsque j'ai veux avoir les jeux videos en utilisant un mot cle, j'obtien les jeux videos qui ont le nom de test3`() {
+
+        //Mise en place
+        var cobaye: List<JeuVideo>
+        var sourceDeDonnées = SourceDeDonnéesBidon()
+
+        //Exécution
+        cobaye = GestionJeuxVideo(sourceDeDonnées).chercherJeuxVideoParMotCle("test3")!!
+
+        //Vérification
+        Assert.assertEquals(1, cobaye.size)
+    }
+
 }
