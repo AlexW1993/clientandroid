c8f916b376eb1af35b2901ca6b8c22bcf6b8948e
mel <0715885@crosemont.qc.ca>
2021-12-08 11:55:48 -0400

    Boutons fonctionnels dans la VueChangementSurnom.kt et changement dans un attribut de fragment_changement_surnom.xml (de text maintenant est hint dans saisie_nouveau_surnom

--- ClientAndroid-Critika/app/src/main/java/com/tp/clientandroid_critika/Présentation/vue/VueChangementSurnom.kt
+++ ClientAndroid-Critika/app/src/main/java/com/tp/clientandroid_critika/Présentation/vue/VueChangementSurnom.kt
@@ -6,8 +6,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.tp.clientandroid_critika.R
+import android.widget.EditText
+import android.widget.ImageButton
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.tp.clientandroid_critika.Présentation.présenteur.PrésentateurMenuCompte
 
 class VueChangementSurnom : Fragment() {
+
+    //private var _présentateur: PrésentateurMenuCompte? = null
+    private var _nav: NavController? = null
+    private var _btnDéconnection: ImageButton? = null
+    private var _btnMenuPrincipale: ImageButton? = null
+    private var _btnRecherche: ImageButton? = null
+    private var _btnCompte: ImageButton? = null
+    private var _nouveauSurnom: EditText? = null
+    private var _btnSauvegarderSurnom: ImageButton? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +35,34 @@ class VueChangementSurnom : Fragment() {
         return inflater.inflate(R.layout.fragment_changement_surnom, container, false)
     }
 
-    companion object {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //_présentateur = PrésentateurMenuCompte(this)
+        _nav = Navigation.findNavController(view)
+        _btnDéconnection = view.findViewById(R.id.bouton_deconnection)
+        _btnMenuPrincipale = view.findViewById(R.id.bouton_menu)
+        _btnRecherche = view.findViewById(R.id.bouton_recherche)
+        _btnCompte = view.findViewById(R.id.bouton_compte)
+        _nouveauSurnom = view.findViewById(R.id.saisie_nouveau_surnom)
+        _btnSauvegarderSurnom = view.findViewById(R.id.bouton_sauvegarde_surnom)
+        _btnDéconnection?.setOnClickListener { view ->
+            _nav!!.navigate(R.id.vuePageInitiale)
+        }
+        _btnMenuPrincipale?.setOnClickListener { view ->
+            _nav!!.navigate(R.id.vueMenuPrincipale)
+        }
+        _btnRecherche?.setOnClickListener { view ->
+            _nav!!.navigate(R.id.vuePageRecherche)
+        }
+        _btnCompte?.setOnClickListener { view ->
+            _nav!!.navigate(R.id.vueMenuCompte)
+        }
+        _btnSauvegarderSurnom?.setOnClickListener { view ->
+
+        }
+
+    }
+        companion object {
 
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
