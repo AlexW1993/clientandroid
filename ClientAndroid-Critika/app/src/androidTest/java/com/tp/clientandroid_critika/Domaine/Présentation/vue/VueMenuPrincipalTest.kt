fe24b961d411ae78e11ba716d6107d982f9e31c2
mel <0715885@crosemont.qc.ca>
2021-12-11 00:14:46 -0400

    Test d'instrumentation VueMenuPrincipalTest.kt

---
+++ ClientAndroid-Critika/app/src/androidTest/java/com/tp/clientandroid_critika/Domaine/Présentation/vue/VueMenuPrincipalTest.kt
@@ -0,0 +1,77 @@
+package com.tp.clientandroid_critika.Domaine.Présentation.vue
+
+import androidx.lifecycle.Lifecycle
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.tp.clientandroid_critika.R
+import com.tp.clientandroid_critika.UI_Activité.MainActivity
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+class VueMenuPrincipalTest {
+
+    private lateinit var scenario: ActivityScenario<MainActivity>
+
+    @Before
+    fun setupTest(){
+        scenario = ActivityScenario.launch(MainActivity::class.java)
+        scenario.moveToState(Lifecycle.State.RESUMED)
+        onView(withId(R.id.bouton_Se_connecter)).perform(click())
+        val surnom = "root"
+        val motDePasse = "root"
+        onView(withId(R.id.zone_texte_surnom)).perform(click())
+        onView(withId(R.id.zone_texte_surnom)).perform(ViewActions.typeText(surnom.toString()))
+        onView(withId(R.id.zone_texte_surnom)).check(matches(withText("root")))
+        Espresso.closeSoftKeyboard()
+        onView(withId(R.id.zone_texte_mot_passe_1)).perform(click())
+        onView(withId(R.id.zone_texte_mot_passe_1)).perform(ViewActions.typeText(motDePasse.toString()))
+        onView(withId(R.id.zone_texte_mot_passe_1)).check(matches(withText("root")))
+        Espresso.closeSoftKeyboard()
+        onView(withId(R.id.bouton_Se_connecter_Connection)).perform(click())
+    }
+
+    @Test
+    fun `TestAffichageMenuPrincipalVisuel`() {
+        onView(withId(R.id.bouton_retour)).check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+        onView(withId(R.id.bouton_deconnection)).check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+        onView(withId(R.id.bouton_menu)).check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+        onView(withId(R.id.bouton_recherche)).check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+        onView(withId(R.id.bouton_compte)).check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+        onView(withId(R.id.recycler_view_menu)).check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+    }
+
+    @Test
+    fun `testBoutonDeconnection`() {
+        onView(withId(R.id.bouton_deconnection)).perform(click())
+    }
+
+    @Test
+    fun `testBoutonMenu`() {
+        onView(withId(R.id.bouton_menu)).perform(click())
+    }
+
+    @Test
+    fun `testBoutonRechercher`() {
+        onView(withId(R.id.bouton_recherche)).perform(click())
+    }
+
+    @Test
+    fun `testBoutonCompte`() {
+        onView(withId(R.id.bouton_compte)).perform(click())
+    }
+
+    //@Test
+    //fun `testBoutonJeu`() {
+    //onView(withId(R.id.button_jeu_menu)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+    //}
+
+}
